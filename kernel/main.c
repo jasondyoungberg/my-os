4423a268c12198b3eb8cfdf15f72c400503a4e26
Jason Youngberg <jasondyoungberg@gmail.com>
2022-12-13 11:07:13 -0700

    Graphics demo

--- kernel/main.c
+++ kernel/main.c
@@ -2,6 +2,7 @@
 #include <stddef.h>
 #include <stdint.h>
 
+#include "graphics.h"
 #include "serial.h"
 
 // Halt the CPU
@@ -23,6 +24,19 @@ void _start(void) {
     serial_println(SERIAL_COM2, "Hello, world! I'm COM2!");
     serial_println(SERIAL_COM3, "Hello, world! I'm COM3!");
     serial_println(SERIAL_COM4, "Hello, world! I'm COM4!");
+
+    // Initialize display
+    graphics_init();
+
+    // Draw a cool pattern
+    for (int i = 0;; i++) {
+        for (int x = 0; x < graphics_width; x++) {
+            for (int y = 0; y < graphics_height; y++) {
+                draw_pix(x, y, color_rgb(x, y, i));
+            }
+        }
+        graphics_refresh();
+    }
 
     halt();
 }
