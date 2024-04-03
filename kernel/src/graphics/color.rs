e94397f5f31a2a5cbdcd3736b7ca22f0eedcb985
Jason Youngberg <jasondyoungberg@gmail.com>
2024-04-02 23:35:30 -0600

    Add color constants to graphics module

--- kernel/src/graphics/color.rs
+++ kernel/src/graphics/color.rs
@@ -10,3 +10,12 @@ impl Color {
         Self { r, g, b }
     }
 }
+
+pub const BLACK: Color = Color::new(0, 0, 0);
+pub const RED: Color = Color::new(255, 0, 0);
+pub const YELLOW: Color = Color::new(255, 255, 0);
+pub const GREEN: Color = Color::new(0, 255, 0);
+pub const CYAN: Color = Color::new(0, 255, 255);
+pub const BLUE: Color = Color::new(0, 0, 255);
+pub const MAGENTA: Color = Color::new(255, 0, 255);
+pub const WHITE: Color = Color::new(255, 255, 255);
--- kernel/src/graphics/mod.rs
+++ kernel/src/graphics/mod.rs
@@ -1,5 +1,5 @@
 mod buffer;
-mod color;
+pub mod color;
 
 pub use buffer::PixelBuffer;
 pub use color::Color;
