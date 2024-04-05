1b381da5f3ef4185878cf6045e09c2d06d06b03d
Jason Youngberg <jasondyoungberg@gmail.com>
2024-04-04 22:56:04 -0600

    Remove commented out code in bitmap.rs

--- kernel/src/graphics/bitmap.rs
+++ kernel/src/graphics/bitmap.rs
@@ -1,17 +1,5 @@
 use super::color::Color;
 
-// pub struct Bitmap<const W: usize, const H: usize>([[Color; W]; H]);
-
-// impl<const W: usize, const H: usize> Bitmap<W, H> {
-//     pub fn new(data: [[Color; W]; H]) -> Self {
-//         Self(data)
-//     }
-
-//     pub fn data(&self) -> &[[Color; W]; H] {
-//         &self.0
-//     }
-// }
-
 pub trait Bitmap {
     fn size(&self) -> (usize, usize);
     fn get_pixel_unchecked(&self, pos: (usize, usize)) -> Color;
