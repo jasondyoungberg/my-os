4161939613df9d6b41d609077c2a0ef1a88da0d4
Jason Youngberg <jasondyoungberg@gmail.com>
2024-04-10 23:30:16 -0600

    Add comment

--- build.rs
+++ build.rs
@@ -1,4 +1,5 @@
 fn main() {
+    // Pass the kernel binary to main.rs
     let kernel = std::env::var_os("CARGO_BIN_FILE_KERNEL_kernel").unwrap();
     println!("cargo::rustc-env=KERNEL_PATH={}", kernel.to_str().unwrap());
 }
