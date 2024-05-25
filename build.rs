1ca5c5026ed2dc529c687fc32418a09cb9136fee
Jason Youngberg <jasondyoungberg@gmail.com>
2024-05-25 14:07:54 -0600

    Create demo app

--- build.rs
+++ build.rs
@@ -4,6 +4,7 @@ use bootloader::DiskImageBuilder;
 
 fn main() {
     const BIN_PREFIX: &str = "CARGO_BIN_FILE_KERNEL_";
+    const APP_PREFIX: &str = "CARGO_BIN_FILE_APP_";
 
     let out_dir = PathBuf::from(std::env::var_os("OUT_DIR").expect("no OUT_DIR env var"));
 
@@ -32,6 +33,28 @@ fn main() {
             let full_path = entry.path();
             let rel_path = full_path.strip_prefix(&files_path).unwrap();
             image.set_file(rel_path.to_str().unwrap().to_string(), full_path);
+        }
+
+        for (app_key, app_value) in env::vars() {
+            if !app_key.starts_with(APP_PREFIX) {
+                continue;
+            }
+
+            let app_name = app_key
+                .chars()
+                .skip_while(|c| c.is_uppercase() || *c == '_')
+                .skip("app_".len())
+                .collect::<String>();
+
+            if app_name.is_empty() {
+                continue;
+            }
+
+            let app_name = format!("{app_name}.app");
+
+            let app_path = PathBuf::from(app_value);
+
+            image.set_file(app_name, app_path);
         }
 
         image
--- stdlib/src/syscall/mod.rs
+++
@@ -1,4 +0,0 @@
-mod raw;
-
-#[cfg(feature = "syscall_raw")]
-pub use raw::*;
