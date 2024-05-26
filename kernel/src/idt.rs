b79b6f9942b9c143e5a51055a273dbe0234d9a42
Jason Youngberg <jasondyoungberg@gmail.com>
2024-05-26 08:15:40 -0600

    Log all pics interrupts as warning

--- kernel/src/idt.rs
+++ kernel/src/idt.rs
@@ -5,7 +5,10 @@ use x86_64::{
     structures::idt::{InterruptDescriptorTable, InterruptStackFrame},
 };
 
-use crate::{lapic, pics::PICS_OFFSET};
+use crate::{
+    lapic,
+    pics::{pics_handler, PICS_OFFSET},
+};
 
 pub static IDT: Lazy<InterruptDescriptorTable> = Lazy::new(|| {
     fn general_handler(stack_frame: InterruptStackFrame, index: u8, error_code: Option<u64>) {
@@ -17,22 +20,7 @@ pub static IDT: Lazy<InterruptDescriptorTable> = Lazy::new(|| {
 
     set_general_handler!(&mut idt, general_handler);
 
-    idt[PICS_OFFSET].set_handler_fn(crate::pics::handle_irq0);
-    idt[PICS_OFFSET + 1].set_handler_fn(crate::pics::handle_irq1);
-    idt[PICS_OFFSET + 2].set_handler_fn(crate::pics::handle_irq2);
-    idt[PICS_OFFSET + 3].set_handler_fn(crate::pics::handle_irq3);
-    idt[PICS_OFFSET + 4].set_handler_fn(crate::pics::handle_irq4);
-    idt[PICS_OFFSET + 5].set_handler_fn(crate::pics::handle_irq5);
-    idt[PICS_OFFSET + 6].set_handler_fn(crate::pics::handle_irq6);
-    idt[PICS_OFFSET + 7].set_handler_fn(crate::pics::handle_irq7);
-    idt[PICS_OFFSET + 8].set_handler_fn(crate::pics::handle_irq8);
-    idt[PICS_OFFSET + 9].set_handler_fn(crate::pics::handle_irq9);
-    idt[PICS_OFFSET + 10].set_handler_fn(crate::pics::handle_irq10);
-    idt[PICS_OFFSET + 11].set_handler_fn(crate::pics::handle_irq11);
-    idt[PICS_OFFSET + 12].set_handler_fn(crate::pics::handle_irq12);
-    idt[PICS_OFFSET + 13].set_handler_fn(crate::pics::handle_irq13);
-    idt[PICS_OFFSET + 14].set_handler_fn(crate::pics::handle_irq14);
-    idt[PICS_OFFSET + 15].set_handler_fn(crate::pics::handle_irq15);
+    set_general_handler!(&mut idt, pics_handler, PICS_OFFSET..=PICS_OFFSET + 16);
 
     idt[lapic::TIMER_VECTOR].set_handler_fn(crate::lapic::handle_timer);
 
--- kernel/src/pics.rs
+++ kernel/src/pics.rs
@@ -1,6 +1,6 @@
 use pic8259::ChainedPics;
 use spin::Mutex;
-use x86_64::structures::idt::InterruptStackFrame;
+use x86_64::{registers::segmentation::GS, structures::idt::InterruptStackFrame};
 
 pub const PICS_OFFSET: u8 = 32;
 
@@ -11,7 +11,7 @@ pub fn init() {
     let mut pics = PICS.lock();
     unsafe {
         pics.initialize();
-        pics.write_masks(0, 0);
+        pics.disable();
     }
 }
 
@@ -22,82 +22,10 @@ fn notify_end_of_interrupt(irq: u8) {
     }
 }
 
-pub extern "x86-interrupt" fn handle_irq0(_stack_frame: InterruptStackFrame) {
-    log::trace!("timer interrupt");
-    notify_end_of_interrupt(0);
-}
-
-pub extern "x86-interrupt" fn handle_irq1(_stack_frame: InterruptStackFrame) {
-    log::trace!("keyboard interrupt");
-    notify_end_of_interrupt(1);
-}
-
-pub extern "x86-interrupt" fn handle_irq2(_stack_frame: InterruptStackFrame) {
-    log::trace!("cascade interrupt");
-    notify_end_of_interrupt(2);
-}
-
-pub extern "x86-interrupt" fn handle_irq3(_stack_frame: InterruptStackFrame) {
-    log::trace!("COM2 interrupt");
-    notify_end_of_interrupt(3);
-}
-
-pub extern "x86-interrupt" fn handle_irq4(_stack_frame: InterruptStackFrame) {
-    log::trace!("COM1 interrupt");
-    notify_end_of_interrupt(4);
-}
-
-pub extern "x86-interrupt" fn handle_irq5(_stack_frame: InterruptStackFrame) {
-    log::trace!("LPT2 interrupt");
-    notify_end_of_interrupt(5);
-}
-
-pub extern "x86-interrupt" fn handle_irq6(_stack_frame: InterruptStackFrame) {
-    log::trace!("floppy disk interrupt");
-    notify_end_of_interrupt(6);
-}
-
-pub extern "x86-interrupt" fn handle_irq7(_stack_frame: InterruptStackFrame) {
-    log::trace!("LPT1 interrupt");
-    notify_end_of_interrupt(7);
-}
-
-pub extern "x86-interrupt" fn handle_irq8(_stack_frame: InterruptStackFrame) {
-    log::trace!("CMOS interrupt");
-    notify_end_of_interrupt(8);
-}
-
-pub extern "x86-interrupt" fn handle_irq9(_stack_frame: InterruptStackFrame) {
-    log::trace!("acpi interrupt");
-    notify_end_of_interrupt(9);
-}
-
-pub extern "x86-interrupt" fn handle_irq10(_stack_frame: InterruptStackFrame) {
-    log::trace!("open interrupt");
-    notify_end_of_interrupt(10);
-}
-
-pub extern "x86-interrupt" fn handle_irq11(_stack_frame: InterruptStackFrame) {
-    log::trace!("open interrupt");
-    notify_end_of_interrupt(11);
-}
-
-pub extern "x86-interrupt" fn handle_irq12(_stack_frame: InterruptStackFrame) {
-    log::trace!("mouse interrupt");
-    notify_end_of_interrupt(12);
-}
-
-pub extern "x86-interrupt" fn handle_irq13(_stack_frame: InterruptStackFrame) {
-    log::trace!("FPU interrupt");
-    notify_end_of_interrupt(13);
-}
-
-pub extern "x86-interrupt" fn handle_irq14(_stack_frame: InterruptStackFrame) {
-    log::trace!("primary ATA interrupt");
-    notify_end_of_interrupt(14);
-}
-
-pub extern "x86-interrupt" fn handle_irq15(_stack_frame: InterruptStackFrame) {
-    log::trace!("secondary ATA interrupt");
-    notify_end_of_interrupt(15);
+pub fn pics_handler(_stack_frame: InterruptStackFrame, index: u8, _error_code: Option<u64>) {
+    unsafe { GS::swap() };
+    log::warn!("PIC interrupt: {}", index - PICS_OFFSET);
+    let irq = index - PICS_OFFSET;
+    crate::pics::notify_end_of_interrupt(irq);
+    unsafe { GS::swap() };
 }
