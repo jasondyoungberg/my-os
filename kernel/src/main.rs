e1674385ecdf7274cb4a0cb5b119a42e5a6b1a7e
Jason Youngberg <jasondyoungberg@gmail.com>
2024-05-25 19:05:34 -0600

    Cleanup code

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -1,18 +1,16 @@
 #![no_std]
 #![no_main]
 #![deny(unsafe_op_in_unsafe_fn)]
+#![allow(dead_code)]
 
 use core::arch::asm;
 
 use limine::{
-    memory_map::EntryType,
     request::{FramebufferRequest, MemoryMapRequest},
     response::FramebufferResponse,
-    smp::Cpu,
     BaseRevision,
 };
 use spin::Lazy;
-use x86_64::registers::control::Cr3;
 
 mod debugcon;
 mod macros;
@@ -55,83 +53,7 @@ extern "C" fn _start() -> ! {
 
     kprintln!("Hello, World!");
 
-    kprintln!("CPUs: {}", SMP_REQUEST.get_response().unwrap().cpus().len());
-
-    kprintln!("Cr3: {:?}", Cr3::read());
-
-    assert!(SMP_REQUEST.get_response().is_some());
-
-    print_memory_map();
-
-    start_cpus();
-
-    if let Some(framebuffer) = FRAMEBUFFER_RESPONSE.framebuffers().next() {
-        for i in 0..100_u64 {
-            // Calculate the pixel offset using the framebuffer information we obtained above.
-            // We skip `i` scanlines (pitch is provided in bytes) and add `i * 4` to skip `i` pixels forward.
-            let pixel_offset = i * framebuffer.pitch() + i * 4;
-
-            // Write 0xFFFFFFFF to the provided pixel offset to fill it white.
-            unsafe { *(framebuffer.addr().add(pixel_offset as usize) as *mut u32) = 0xFFFFFFFF };
-        }
-    }
-
     hcf();
-}
-
-extern "C" fn _start_core(cpu: &Cpu) -> ! {
-    kprintln!("CPU{} started!", cpu.id);
-    hcf();
-}
-
-fn start_cpus() {
-    for cpu in SMP_RESPONSE.cpus() {
-        if cpu.id == 0 {
-            continue;
-        }
-
-        kprintln!("Starting CPU{}...", cpu.id);
-
-        cpu.goto_address.write(_start_core);
-    }
-}
-
-fn print_memory_map() {
-    kprintln!("Memory map:");
-
-    let mut reclaimable = 0;
-    let mut usable = 0;
-
-    for entry in MEMORY_MAP_RESPONSE.entries() {
-        let base = entry.base;
-        let length = entry.length;
-        let end = base + length - 1;
-        let entry_type = entry.entry_type;
-        kprintln!(
-            "{base:8x} - {end:8x} : {:?}",
-            match entry_type {
-                EntryType::USABLE => "Usable",
-                EntryType::RESERVED => "Reserved",
-                EntryType::ACPI_RECLAIMABLE => "ACPI Reclaimable",
-                EntryType::ACPI_NVS => "ACPI NVS",
-                EntryType::BAD_MEMORY => "Bad Memory",
-                EntryType::BOOTLOADER_RECLAIMABLE => "Bootloader Reclaimable",
-                EntryType::KERNEL_AND_MODULES => "Kernel and Modules",
-                EntryType::FRAMEBUFFER => "Framebuffer",
-                _ => "Unknown",
-            }
-        );
-
-        match entry_type {
-            EntryType::USABLE => usable += length,
-            EntryType::ACPI_RECLAIMABLE => reclaimable += length,
-            EntryType::BOOTLOADER_RECLAIMABLE => reclaimable += length,
-            _ => {}
-        }
-    }
-
-    kprintln!("Usable memory: {} KiB", usable / 1024);
-    kprintln!("Reclaimable memory: {} KiB", reclaimable / 1024);
 }
 
 #[panic_handler]
