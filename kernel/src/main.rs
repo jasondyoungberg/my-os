f2066b573768fb417bbdacbe4b330a2cdcfe4e2c
Jason Youngberg <jasondyoungberg@gmail.com>
2024-05-29 14:28:29 -0600

    renable apps

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -85,12 +85,12 @@ extern "C" fn init_cpu(cpu: &Cpu) -> ! {
         let hello = read_file(find_file("/hello.txt"));
         print!("{}", str::from_utf8(hello).unwrap());
         let mut manager = MANAGER.get().unwrap().lock();
-        // manager.spawn(include_bytes!("../app/hello"));
+        manager.spawn(include_bytes!("../app/hello"));
         manager.spawn(include_bytes!("../app/preservation"));
-        // manager.spawn(include_bytes!("../app/loop"));
-        // manager.spawn(include_bytes!("../app/yeild"));
-        // manager.spawn(include_bytes!("../app/stack"));
-        // manager.spawn(include_bytes!("../app/segfault"));
+        manager.spawn(include_bytes!("../app/loop"));
+        manager.spawn(include_bytes!("../app/yeild"));
+        manager.spawn(include_bytes!("../app/stack"));
+        manager.spawn(include_bytes!("../app/segfault"));
         manager.spawn(include_bytes!("../../app/hello/dist/hello.bin"));
     }
 
