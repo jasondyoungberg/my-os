957e9dfb1b25718e9ee9458ae890898176396429
Jason Youngberg <jasondyoungberg@gmail.com>
2024-05-30 08:30:08 -0600

    Get bump allocator working

--- kernel/build.rs
+++ kernel/build.rs
@@ -1,6 +1,8 @@
 fn main() {
-    // Tell cargo to pass the linker script to the linker..
-    println!("cargo:rustc-link-arg=-Tlinker.ld");
+    if std::env::var("CARGO_CFG_TARGET_OS").unwrap() == "none" {
+        // Tell cargo to pass the linker script to the linker..
+        println!("cargo:rustc-link-arg=-Tlinker.ld");
+    }
     // ..and to re-run if it changes.
     println!("cargo:rerun-if-changed=linker.ld");
 }
--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -1,12 +1,16 @@
 #![no_std]
-#![no_main]
+#![cfg_attr(not(test), no_main)]
 //
 #![allow(dead_code)]
 #![deny(unsafe_op_in_unsafe_fn)]
 
+extern crate alloc;
+
+use alloc::boxed::Box;
 use drivers::display;
 
 mod drivers;
+mod heap;
 mod instructions;
 mod limine;
 
@@ -18,7 +22,7 @@ static BASE_REVISION: limine::BaseRevision = limine::BaseRevision::new();
 #[link_section = ".requests"]
 static FRAMEBUFFER_REQUEST: limine::FramebufferRequest = limine::FramebufferRequest::new();
 
-#[no_mangle]
+#[cfg_attr(not(test), no_mangle)]
 extern "C" fn _start() -> ! {
     assert!(BASE_REVISION.is_supported());
     assert!(FRAMEBUFFER_REQUEST.response.get().is_some());
@@ -49,7 +53,7 @@ extern "C" fn _start() -> ! {
     }
 }
 
-#[panic_handler]
+#[cfg_attr(not(test), panic_handler)]
 fn rust_panic(info: &core::panic::PanicInfo) -> ! {
     instructions::disable_interrupts();
     println!("{}", info);
