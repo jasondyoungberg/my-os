a5cecbca025c8c5a0565817955ff859254505bb1
Jason Youngberg <jasondyoungberg@gmail.com>
2024-06-06 10:18:38 -0600

    Add format stuff

--- kernel/watch.sh
+++ kernel/watch.sh
@@ -1,7 +1,8 @@
+#!/bin/bash
 
-clear; make -s clean all
+clear; make -s all tidy
 
 inotifywait --monitor --quiet --recursive --event close_write ./src |
 while read -r filename event; do
-    clear; make -s clean all
+    clear; make -s all tidy
 done
