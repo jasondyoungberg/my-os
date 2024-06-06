8a27723f9cb17711d31398dbf0a7a738ae32e2d7
Jason Youngberg <jasondyoungberg@gmail.com>
2024-06-06 16:09:49 -0600

    Better ubsan handlers

--- kernel/src/ubsan.c
+++ kernel/src/ubsan.c
@@ -19,10 +19,8 @@
  * Undefined behavior sanitizer runtime support.
  */
 
+#include "common/log.h"
 #include <stdint.h>
-
-void __ubsan_handle_type_mismatch_v1(void* data_raw, void* pointer_raw) {}
-void __ubsan_handle_pointer_overflow(void* data_raw, void* pointer_raw) {}
 
 struct ubsan_source_location {
     const char* filename;
@@ -38,36 +36,6 @@ struct ubsan_type_descriptor {
 
 typedef uintptr_t ubsan_value_handle_t;
 
-static const struct ubsan_source_location unknown_location = {
-    "<unknown file>",
-    0,
-    0,
-};
-
-__attribute__((noreturn)) static void
-ubsan_abort(const struct ubsan_source_location* location,
-            const char* violation) {
-    if (!location || !location->filename)
-        location = &unknown_location;
-
-    panic("UBSAN: %s @ %s:%u:%u", violation, location->filename, location->line,
-          location->column);
-}
-
-#define ABORT_VARIANT(name, params, call)                                      \
-    __attribute__((noreturn)) void __ubsan_handle_##name##_abort params {      \
-        __ubsan_handle_##name call;                                            \
-        __builtin_unreachable();                                               \
-    }
-
-#define ABORT_VARIANT_VP(name) ABORT_VARIANT(name, (void* a), (a))
-#define ABORT_VARIANT_VP_VP(name)                                              \
-    ABORT_VARIANT(name, (void* a, void* b), (a, b))
-#define ABORT_VARIANT_VP_IP(name)                                              \
-    ABORT_VARIANT(name, (void* a, intptr_t b), (a, b))
-#define ABORT_VARIANT_VP_VP_VP(name)                                           \
-    ABORT_VARIANT(name, (void* a, void* b, void* c), (a, b, c))
-
 struct ubsan_type_mismatch_data {
     struct ubsan_source_location location;
     struct ubsan_type_descriptor* type;
@@ -75,78 +43,71 @@ struct ubsan_type_mismatch_data {
     unsigned char type_check_kind;
 };
 
-void __ubsan_handle_type_mismatch(void* data_raw, void* pointer_raw) {
-    struct ubsan_type_mismatch_data* data =
-        (struct ubsan_type_mismatch_data*)data_raw;
-    ubsan_value_handle_t pointer = (ubsan_value_handle_t)pointer_raw;
+void __ubsan_handle_type_mismatch_v1(struct ubsan_type_mismatch_data* data,
+                                     ubsan_value_handle_t value) {
     const char* violation = "type mismatch";
-    if (!pointer)
-        violation = "null pointer access";
-    else if (data->alignment && (pointer & (data->alignment - 1)))
+    if (!value)
+        violation = "type mismatch";
+    else if (data->alignment && (value & (data->alignment - 1)))
         violation = "unaligned access";
-    ubsan_abort(&data->location, violation);
-}
 
-ABORT_VARIANT_VP_VP(type_mismatch);
+    log_error("%s @ %s:%u:%u (%x)", violation, data->location.filename,
+              data->location.line, data->location.column, value);
+}
 
 struct ubsan_overflow_data {
     struct ubsan_source_location location;
     struct ubsan_type_descriptor* type;
 };
 
-void __ubsan_handle_add_overflow(void* data_raw, void* lhs_raw, void* rhs_raw) {
-    struct ubsan_overflow_data* data = (struct ubsan_overflow_data*)data_raw;
-    ubsan_value_handle_t lhs = (ubsan_value_handle_t)lhs_raw;
-    ubsan_value_handle_t rhs = (ubsan_value_handle_t)rhs_raw;
-    (void)lhs;
-    (void)rhs;
-    ubsan_abort(&data->location, "addition overflow");
+void __ubsan_handle_add_overflow(struct ubsan_overflow_data* data,
+                                 ubsan_value_handle_t lhs,
+                                 ubsan_value_handle_t rhs) {
+    log_error("addition overflow @ %s:%u:%u (%x %x)", data->location.filename,
+              data->location.line, data->location.column, lhs, rhs);
 }
 
-ABORT_VARIANT_VP_VP_VP(add_overflow);
-
-void __ubsan_handle_sub_overflow(void* data_raw, void* lhs_raw, void* rhs_raw) {
-    struct ubsan_overflow_data* data = (struct ubsan_overflow_data*)data_raw;
-    ubsan_value_handle_t lhs = (ubsan_value_handle_t)lhs_raw;
-    ubsan_value_handle_t rhs = (ubsan_value_handle_t)rhs_raw;
-    (void)lhs;
-    (void)rhs;
-    ubsan_abort(&data->location, "subtraction overflow");
+void __ubsan_handle_sub_overflow(struct ubsan_overflow_data* data,
+                                 ubsan_value_handle_t lhs,
+                                 ubsan_value_handle_t rhs) {
+    log_error("subtraction overflow @ %s:%u:%u (%x %x)",
+              data->location.filename, data->location.line,
+              data->location.column, lhs, rhs);
 }
 
-ABORT_VARIANT_VP_VP_VP(sub_overflow);
-
-void __ubsan_handle_mul_overflow(void* data_raw, void* lhs_raw, void* rhs_raw) {
-    struct ubsan_overflow_data* data = (struct ubsan_overflow_data*)data_raw;
-    ubsan_value_handle_t lhs = (ubsan_value_handle_t)lhs_raw;
-    ubsan_value_handle_t rhs = (ubsan_value_handle_t)rhs_raw;
-    (void)lhs;
-    (void)rhs;
-    ubsan_abort(&data->location, "multiplication overflow");
+void __ubsan_handle_mul_overflow(struct ubsan_overflow_data* data,
+                                 ubsan_value_handle_t lhs,
+                                 ubsan_value_handle_t rhs) {
+    log_error("multiplication overflow @ %s:%u:%u (%x %x)",
+              data->location.filename, data->location.line,
+              data->location.column, lhs, rhs);
 }
 
-ABORT_VARIANT_VP_VP_VP(mul_overflow);
-
-void __ubsan_handle_negate_overflow(void* data_raw, void* old_value_raw) {
-    struct ubsan_overflow_data* data = (struct ubsan_overflow_data*)data_raw;
-    ubsan_value_handle_t old_value = (ubsan_value_handle_t)old_value_raw;
-    (void)old_value;
-    ubsan_abort(&data->location, "negation overflow");
+void __ubsan_handle_negate_overflow(struct ubsan_overflow_data* data,
+                                    ubsan_value_handle_t old_value) {
+    log_error("negation overflow @ %s:%u:%u (%x)", data->location.filename,
+              data->location.line, data->location.column, old_value);
 }
 
-ABORT_VARIANT_VP_VP(negate_overflow);
-
-void __ubsan_handle_divrem_overflow(void* data_raw, void* lhs_raw,
-                                    void* rhs_raw) {
-    struct ubsan_overflow_data* data = (struct ubsan_overflow_data*)data_raw;
-    ubsan_value_handle_t lhs = (ubsan_value_handle_t)lhs_raw;
-    ubsan_value_handle_t rhs = (ubsan_value_handle_t)rhs_raw;
-    (void)lhs;
-    (void)rhs;
-    ubsan_abort(&data->location, "division remainder overflow");
+void __ubsan_handle_divrem_overflow(struct ubsan_overflow_data* data,
+                                    ubsan_value_handle_t lhs,
+                                    ubsan_value_handle_t rhs) {
+    log_error("division remainder overflow @ %s:%u:%u (%x %x)",
+              data->location.filename, data->location.line,
+              data->location.column, lhs, rhs);
 }
 
-ABORT_VARIANT_VP_VP_VP(divrem_overflow);
+struct ubsan_pointer_overflow_data {
+    struct ubsan_source_location location;
+};
+
+void __ubsan_handle_pointer_overflow(struct ubsan_pointer_overflow_data* data,
+                                     ubsan_value_handle_t base,
+                                     ubsan_value_handle_t result) {
+    log_error("division remainder overflow @ %s:%u:%u (%x %x)",
+              data->location.filename, data->location.line,
+              data->location.column, base, result);
+}
 
 struct ubsan_shift_out_of_bounds_data {
     struct ubsan_source_location location;
@@ -154,18 +115,12 @@ struct ubsan_shift_out_of_bounds_data {
     struct ubsan_type_descriptor* rhs_type;
 };
 
-void __ubsan_handle_shift_out_of_bounds(void* data_raw, void* lhs_raw,
-                                        void* rhs_raw) {
-    struct ubsan_shift_out_of_bounds_data* data =
-        (struct ubsan_shift_out_of_bounds_data*)data_raw;
-    ubsan_value_handle_t lhs = (ubsan_value_handle_t)lhs_raw;
-    ubsan_value_handle_t rhs = (ubsan_value_handle_t)rhs_raw;
-    (void)lhs;
-    (void)rhs;
-    ubsan_abort(&data->location, "shift out of bounds");
+void __ubsan_handle_shift_out_of_bounds(
+    struct ubsan_shift_out_of_bounds_data* data, ubsan_value_handle_t lhs,
+    ubsan_value_handle_t rhs) {
+    log_error("shift out of bounds @ %s:%u:%u (%x %x)", data->location.filename,
+              data->location.line, data->location.column, lhs, rhs);
 }
-
-ABORT_VARIANT_VP_VP_VP(shift_out_of_bounds);
 
 struct ubsan_out_of_bounds_data {
     struct ubsan_source_location location;
@@ -173,117 +128,60 @@ struct ubsan_out_of_bounds_data {
     struct ubsan_type_descriptor* index_type;
 };
 
-void __ubsan_handle_out_of_bounds(void* data_raw, void* index_raw) {
-    struct ubsan_out_of_bounds_data* data =
-        (struct ubsan_out_of_bounds_data*)data_raw;
-    ubsan_value_handle_t index = (ubsan_value_handle_t)index_raw;
-    (void)index;
-    ubsan_abort(&data->location, "out of bounds");
+void __ubsan_handle_out_of_bounds(struct ubsan_out_of_bounds_data* data,
+                                  ubsan_value_handle_t index) {
+    log_error("out of bounds @ %s:%u:%u (%x)", data->location.filename,
+              data->location.line, data->location.column, index);
 }
-
-ABORT_VARIANT_VP_VP(out_of_bounds);
 
 struct ubsan_unreachable_data {
     struct ubsan_source_location location;
 };
 
 __attribute__((noreturn)) void
-__ubsan_handle_builtin_unreachable(void* data_raw) {
-    struct ubsan_unreachable_data* data =
-        (struct ubsan_unreachable_data*)data_raw;
-    ubsan_abort(&data->location, "reached unreachable");
+__ubsan_handle_builtin_unreachable(struct ubsan_unreachable_data* data) {
+    panic("reached unreachable @ %s:%u:%u", data->location.filename,
+          data->location.line, data->location.column);
 }
 
-__attribute__((noreturn)) void __ubsan_handle_missing_return(void* data_raw) {
-    struct ubsan_unreachable_data* data =
-        (struct ubsan_unreachable_data*)data_raw;
-    ubsan_abort(&data->location, "missing return");
+__attribute__((noreturn)) void
+__ubsan_handle_missing_return(struct ubsan_unreachable_data* data) {
+    panic("missing return @ %s:%u:%u", data->location.filename,
+          data->location.line, data->location.column);
 }
-
-struct ubsan_vla_bound_data {
-    struct ubsan_source_location location;
-    struct ubsan_type_descriptor* type;
-};
-
-void __ubsan_handle_vla_bound_not_positive(void* data_raw, void* bound_raw) {
-    struct ubsan_vla_bound_data* data = (struct ubsan_vla_bound_data*)data_raw;
-    ubsan_value_handle_t bound = (ubsan_value_handle_t)bound_raw;
-    (void)bound;
-    ubsan_abort(&data->location, "negative variable array length");
-}
-
-ABORT_VARIANT_VP_VP(vla_bound_not_positive);
-
-struct ubsan_float_cast_overflow_data {
-    // TODO: Remove this GCC 5.x compatibility after switching to GCC 6.x.
-    // The
-    //       GCC developers accidentally forgot the source location. Their
-    //       libubsan probes to see if it looks like a path, but we don't
-    //       need to maintain compatibility with multiple gcc releases. See
-    //       below.
-#if !(defined(__GNUC__) && __GNUC__ < 6)
-    struct ubsan_source_location location;
-#endif
-    struct ubsan_type_descriptor* from_type;
-    struct ubsan_type_descriptor* to_type;
-};
-
-void __ubsan_handle_float_cast_overflow(void* data_raw, void* from_raw) {
-    struct ubsan_float_cast_overflow_data* data =
-        (struct ubsan_float_cast_overflow_data*)data_raw;
-    ubsan_value_handle_t from = (ubsan_value_handle_t)from_raw;
-    (void)from;
-#if !(defined(__GNUC__) && __GNUC__ < 6)
-    ubsan_abort(&data->location, "float cast overflow");
-#else
-    ubsan_abort(((void)data, &unknown_location), "float cast overflow");
-#endif
-}
-
-ABORT_VARIANT_VP_VP(float_cast_overflow);
 
 struct ubsan_invalid_value_data {
     struct ubsan_source_location location;
     struct ubsan_type_descriptor* type;
 };
 
-void __ubsan_handle_load_invalid_value(void* data_raw, void* value_raw) {
-    struct ubsan_invalid_value_data* data =
-        (struct ubsan_invalid_value_data*)data_raw;
-    ubsan_value_handle_t value = (ubsan_value_handle_t)value_raw;
-    (void)value;
-    ubsan_abort(&data->location, "invalid value load");
+void __ubsan_handle_load_invalid_value(struct ubsan_invalid_value_data* data,
+                                       ubsan_value_handle_t value) {
+    log_error("invalid value load @ %s:%u:%u (%x)", data->location.filename,
+              data->location.line, data->location.column, value);
 }
-
-ABORT_VARIANT_VP_VP(load_invalid_value);
 
 struct ubsan_function_type_mismatch_data {
     struct ubsan_source_location location;
     struct ubsan_type_descriptor* type;
 };
 
-void __ubsan_handle_function_type_mismatch(void* data_raw, void* value_raw) {
-    struct ubsan_function_type_mismatch_data* data =
-        (struct ubsan_function_type_mismatch_data*)data_raw;
-    ubsan_value_handle_t value = (ubsan_value_handle_t)value_raw;
-    (void)value;
-    ubsan_abort(&data->location, "function type mismatch");
+void __ubsan_handle_function_type_mismatch(
+    struct ubsan_function_type_mismatch_data* data,
+    ubsan_value_handle_t value) {
+    log_error("function type mismatch @ %s:%u:%u (%x)", data->location.filename,
+              data->location.line, data->location.column, value);
 }
-
-ABORT_VARIANT_VP_VP(function_type_mismatch);
 
 struct ubsan_nonnull_return_data {
     struct ubsan_source_location location;
     struct ubsan_source_location attr_location;
 };
 
-void __ubsan_handle_nonnull_return(void* data_raw) {
-    struct ubsan_nonnull_return_data* data =
-        (struct ubsan_nonnull_return_data*)data_raw;
-    ubsan_abort(&data->location, "null return");
+void __ubsan_handle_nonnull_return(struct ubsan_nonnull_return_data* data) {
+    log_error("null return @ %s:%u:%u", data->location.filename,
+              data->location.line, data->location.column);
 }
-
-ABORT_VARIANT_VP(nonnull_return);
 
 struct ubsan_nonnull_arg_data {
     struct ubsan_source_location location;
@@ -292,28 +190,21 @@ struct ubsan_nonnull_arg_data {
 
 // TODO: GCC's libubsan does not have the second parameter, but its builtin
 //       somehow has it and conflict if we don't match it.
-void __ubsan_handle_nonnull_arg(void* data_raw, intptr_t index_raw) {
-    struct ubsan_nonnull_arg_data* data =
-        (struct ubsan_nonnull_arg_data*)data_raw;
-    ubsan_value_handle_t index = (ubsan_value_handle_t)index_raw;
-    (void)index;
-    ubsan_abort(&data->location, "null argument");
+void __ubsan_handle_nonnull_arg(struct ubsan_nonnull_arg_data* data,
+                                ubsan_value_handle_t index) {
+    log_error("null argument @ %s:%u:%u (%x)", data->location.filename,
+              data->location.line, data->location.column, index);
 }
-
-ABORT_VARIANT_VP_IP(nonnull_arg);
 
 struct ubsan_cfi_bad_icall_data {
     struct ubsan_source_location location;
     struct ubsan_type_descriptor* type;
 };
 
-void __ubsan_handle_cfi_bad_icall(void* data_raw, void* value_raw) {
-    struct ubsan_cfi_bad_icall_data* data =
-        (struct ubsan_cfi_bad_icall_data*)data_raw;
-    ubsan_value_handle_t value = (ubsan_value_handle_t)value_raw;
-    (void)value;
-    ubsan_abort(&data->location,
-                "control flow integrity check failure during indirect call");
+void __ubsan_handle_cfi_bad_icall(struct ubsan_cfi_bad_icall_data* data,
+                                  ubsan_value_handle_t value) {
+    log_error("control flow integrity check failure during indirect call @ "
+              "%s:%u:%u (%x)",
+              data->location.filename, data->location.line,
+              data->location.column, value);
 }
-
-ABORT_VARIANT_VP_VP(cfi_bad_icall);
