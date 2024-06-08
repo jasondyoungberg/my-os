d44e76f370e0cf6b0c27f4246b12f3565056482b
Jason Youngberg <jasondyoungberg@gmail.com>
2024-06-08 15:56:29 -0600

    Restart

--- kernel/build.rs
+++
@@ -1,8 +0,0 @@
-fn main() {
-    if std::env::var("CARGO_CFG_TARGET_OS").unwrap() == "none" {
-        // Tell cargo to pass the linker script to the linker..
-        println!("cargo:rustc-link-arg=-Tlinker.ld");
-    }
-    // ..and to re-run if it changes.
-    println!("cargo:rerun-if-changed=linker.ld");
-}
--- kernel/src/drivers/mod.rs
+++
@@ -1,9 +0,0 @@
-pub mod acpi;
-pub mod debug_console;
-pub mod display;
-pub mod ioapic;
-pub mod lapic;
-pub mod pic;
-pub mod ps2;
-pub mod psf2;
-pub mod video_console;
